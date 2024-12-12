b6da5c52c31159255cbaa6b0359712914049d416
eelcovdw <eelcovdw@gmail.com>
2024-12-12 13:21:40 +0100

    update

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ import shutil
 from pathlib import Path
 
 from syftbox.lib import Client, SyftPermission
-from syftbox.lib.constants import PERM_FILE
 
 from constants import BROADCAST_ENABLED
 from utils import create_symlink, start_garbage_collector, start_notification_service
@@ -21,12 +20,11 @@ rejected_symplink_path = my_inbox_path / "rejected"
 client.makedirs(my_inbox_path, trash_path)
 
 # Make the inbox path globally writeable
-permission_file = my_inbox_path / PERM_FILE
 permission = SyftPermission.mine_with_public_write(
-    email=client.email,
-    filepath=permission_file.relative_to(client.datasites),
+    context=client,
+    dir=my_inbox_path,
 )
-permission.ensure(path=permission_file)
+permission.ensure(my_inbox_path)
 
 # Create a symlink called "approved" in inbox, pointing to the apps folder
 create_symlink(my_apps_path, approved_symlink_path, overwrite=True)
