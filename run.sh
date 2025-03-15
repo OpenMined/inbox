bfe71061c4882a3a8d424f7f902af29ac586a63a
Ionesio Junior <ionesiojr@gmail.com>
2025-03-15 11:49:40 -0300

    feat: refactor run.sh to loop for virtual environment setup and execution

--- run.sh
+++ run.sh
@@ -1,19 +1,24 @@
 #!/bin/sh
 set -e
 
-if [ ! -d ".venv" ]; then
-    echo "Virtual environment not found. Creating one..."
-    uv venv -p 3.12 .venv
-    echo "Virtual environment created successfully."
-else
-    echo "Virtual environment already exists."
-fi
+while true; do
+    if [ ! -d ".venv" ]; then
+        echo "Virtual environment not found. Creating one..."
+        uv venv -p 3.12 .venv
+        echo "Virtual environment created successfully."
+    else
+        echo "Virtual environment already exists."
+    fi
 
-uv pip install -U syftbox
+    uv pip install -U syftbox
 
-. .venv/bin/activate
+    . .venv/bin/activate
 
-echo "Running 'inbox' with $(python3 --version) at '$(which python3)'"
-uv run python3 main.py
+    echo "Running 'inbox' with $(python3 --version) at '$(which python3)'"
+    uv run python3 main.py
 
-deactivate
+    deactivate
+
+    echo "Sleeping for 10 seconds..."
+    sleep 10
+done
