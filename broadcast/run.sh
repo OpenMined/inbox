42e16cbf6b740b43d37c5ffe382cfff436fd765b
Yash Gorana <yash.gorana@hotmail.com>
2025-03-17 14:00:08 +0530

    run.sh - setup once

--- broadcast/run.sh
+++ broadcast/run.sh
@@ -1,19 +1,16 @@
 #!/bin/sh
 set -e
 
-if [ ! -d ".venv" ]; then
-    echo "Virtual environment not found. Creating one..."
-    uv venv -p 3.12 .venv
-    echo "Virtual environment created successfully."
-else
-    echo "Virtual environment already exists."
-fi
-
+uv venv -p 3.12 .venv
 uv pip install -U syftbox
-
 . .venv/bin/activate
 
-echo "Running 'broadcast' with $(python3 --version) at '$(which python3)'"
-uv run python3 main.py
+while true; do
+    echo "Running 'broadcast' with $(python3 --version) at '$(which python3)'"
+    uv run python3 main.py
+
+    echo "Sleeping for 10 seconds..."
+    sleep 10
+done
 
 deactivate
